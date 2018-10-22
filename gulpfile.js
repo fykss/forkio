bad76db6c156c714a32b10bd271249871e58be50
Влад Летун <fyks1649@gmail.com>
2018-10-22 17:49:41 +0300

    2 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-"use strict"
+"use strict";
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync').create()
-var clean = require('gulp-clean')
-var autoprefixer = require('gulp-autoprefixer')
-var concat = require('gulp-concat')
-var minify = require('gulp-terser')
-var csso = require('gulp-csso')
-var sourceMap = require ('gulp-sourcemaps')
-var imagemin = require('gulp-imagemin')
-var runSequence = require('run-sequence')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var rigger = require('gulp-rigger');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var minify = require('gulp-terser');
+var csso = require('gulp-csso');
+var sourceMap = require ('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
 
 gulp.task('clean', function () {
     return gulp.src('./dist', {read:false})
         .pipe(clean())
-})
+});
+
+
 
 gulp.task('css', function () {
     return gulp.src('./src/scss/**/*.scss')
@@ -29,14 +32,14 @@ gulp.task('css', function () {
         .pipe(csso())
         .pipe(sourceMap.write())
         .pipe(gulp.dest('./dist/css'));
-})
+});
 
 gulp.task('scripts', function () {
     return gulp.src('./src/js/**/*.js')
             .pipe(concat('script.min.js'))
             .pipe( minify())
             .pipe( gulp.dest('./dist/js'))
-})
+});
 
 gulp.task('img', function(){
     return gulp.src('./src/img/**/*')
@@ -46,21 +49,27 @@ gulp.task('img', function(){
             svgoPlugins: [{removeViewBox:false}],
         }))
         .pipe(gulp.dest('./dist/img'))
-})
+});
+
+gulp.task('html', function () {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.dist.server))
+});
 
 gulp.task('dev', function () {
     return runSequence('clean',['css','scripts','img'], function () {
         browserSync.init({
             server:'./'
-        })
+        });
         gulp.watch('./src/scss/**/*.scss',['css']).on('change', browserSync.reload);
         gulp.watch('./src/js/**/*.js',['scripts']).on('change', browserSync.reload);
         gulp.watch('./index.html').on('change', browserSync.reload);
     })
-})
+});
 
 gulp.task('build', function () {
     return runSequence('clean',['css','scripts','img'],function(){
 
     })
-})
+});
